60efdd3e392c0a4ff3ae080a00b75fe2a655405a
Jonathan Singh <jmsingh9899@gmail.com>
2021-11-29 00:01:32 -0800

    made the graph routes

--- controllers/api/workoutRoutes.js
+++ controllers/api/workoutRoutes.js
@@ -10,4 +10,14 @@ router.get('/', (req, res) => {
         })
 })
 
+router.get('/range', (req, res) => {
+    db.Workout.find({}).sort({day:-1})
+    
+    .then(data=>{
+        res.json(data)
+    }).catch(err=>{
+        console.log(err)
+        res.json(err)
+    })
+})
 module.exports = router
